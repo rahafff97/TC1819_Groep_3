27a2b70d75671937c4f82eb951df0576958603c2
jagmeet44 <jagmeet44@live.nl>
2019-03-16 21:46:22 +0100

    Add files via upload

---
+++ src/main/java/com/example/borrow/FourColumn_ListAdapter.java
@@ -0,0 +1,59 @@
+package com.example.borrow;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+
+public class FourColumn_ListAdapter extends ArrayAdapter<Item> {
+
+    private LayoutInflater mInflater;
+    private ArrayList<Item> items;
+    private int mViewResourceID;
+
+    public FourColumn_ListAdapter(Context context, int textViewResourceId,ArrayList<Item> items){
+        super(context,textViewResourceId,items);
+        this.items = items;
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mViewResourceID = textViewResourceId;
+
+    }
+    public View getView(int position, View convertView, ViewGroup parent){
+        convertView = mInflater.inflate(mViewResourceID,null);
+
+        Item item = items.get(position);
+
+        if(item != null){
+            TextView ItemName = (TextView) convertView.findViewById(R.id.eItem);
+            TextView ItemCategorie = (TextView) convertView.findViewById(R.id.eItemcat);
+            TextView ItemDescription = (TextView) convertView.findViewById(R.id.eItemdes);
+            TextView ItemQuantity = (TextView) convertView.findViewById(R.id.eItemq);
+
+
+
+            if(ItemName != null){
+                ItemName.setText((item.getItem()));
+
+            }
+            if(ItemCategorie != null){
+                ItemCategorie.setText((item.getItemCategorie()));
+
+            }
+            if(ItemDescription != null){
+                ItemDescription.setText((item.getItemDescription()));
+
+            }
+            if(ItemQuantity != null){
+                ItemQuantity.setText(Integer.toString(item.getItemQuantity()));
+
+            }
+        }
+        return convertView;
+    }
+
+}
---
+++ src/main/java/com/example/borrow/User.java
@@ -0,0 +1,11 @@
+package com.example.borrow;
+
+public class User {
+
+    private String ID;
+    private String UserName;
+    private String UserTrust;
+    private int UserAge;
+
+
+}
