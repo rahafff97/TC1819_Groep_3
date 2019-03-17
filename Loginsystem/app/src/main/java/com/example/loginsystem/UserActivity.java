e14b94702b68b7d9538b7e20b05e452d00f5cde4
Rahafff97 <rahafffrahaf97@gmail.com>
2019-03-17 19:12:25 +0100

    user_layout aanpassen

--- Loginsystem/app/src/main/java/com/example/loginsystem/UserActivity.java
+++ Loginsystem/app/src/main/java/com/example/loginsystem/UserActivity.java
@@ -1,5 +1,4 @@
 package com.example.loginsystem;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,21 +10,58 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.bumptech.glide.Glide;
+import com.example.loginsystem.LoginActivity;
+import com.example.loginsystem.R;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-
 public class UserActivity extends AppCompatActivity {
+    GoogleSignInClient mGoogleSignClient;
     Button sign_out;
+    TextView nameTV;
+    TextView emailTV;
+    TextView idTV;
+    ImageView photoIV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
         sign_out = findViewById(R.id.log_out);
+        nameTV = findViewById(R.id.name);
+        emailTV = findViewById(R.id.email);
+        idTV = findViewById(R.id.id);
+        photoIV = findViewById(R.id.photo);
+
+        /* Configure sign-in to request the user's ID, email address, and basic*/
+        /* profile. ID and basic profile are included in DEFAULT_SIGN_IN.*/
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignClient = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(UserActivity.this);
+        //check Hr account or not
+        String personEmail = acct.getEmail();
+        if (acct != null && personEmail.endsWith("@hr.nl")) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+
+            nameTV.setText("Name: "+personName);
+            emailTV.setText("Email: "+personEmail);
+            idTV.setText("ID: "+personId);
+            Glide.with(this).load(personPhoto).into(photoIV);
+        }
+
 
         sign_out.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,9 +71,17 @@ public class UserActivity extends AppCompatActivity {
 
         });
     }
+
     private void signOut() {
+        mGoogleSignClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(UserActivity.this, "Succesfully signed out", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(UserActivity.this, LoginActivity.class));
+                    }
+                });
         Toast.makeText(UserActivity.this, "Succesfully signed out", Toast.LENGTH_SHORT).show();
         startActivity(new Intent(UserActivity.this, LoginActivity.class));
     }
 }
-
