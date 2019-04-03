f7fc1cdf5ca4d0a39f7c98a7a33926b2a3535c63
Albert <0961988@hr.nl>
2019-04-03 14:40:43 +0200

    Changed urls, Added base_url, Changed databse

--- Django/TechLab/TechLab/settings/base.py
+++ Django/TechLab/TechLab/settings/base.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+BASE_URL = "http://localhost:8000/"
+
 ALLOWED_HOSTS = ['*']
 
 
