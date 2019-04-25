d6e442a46988e417fd3d842804ebb962a9a148e0
Gizemsaz <g.sazak@live.nl>
2019-04-25 09:45:48 +0200

    -

--- Android/TechLab/app/src/main/java/nl/group3/techlab/MainActivity.java
+++ Android/TechLab/app/src/main/java/nl/group3/techlab/MainActivity.java
@@ -2,6 +2,7 @@ package nl.group3.techlab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.borrow.R;
 
 public class MainActivity extends AppCompatActivity {
 
